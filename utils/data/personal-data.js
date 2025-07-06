bd0512a7531512702c59058afcf15b92ee950802
OsamaIbrhim <osamaibrhiim@gmail.com>
2025-07-06 20:01:22 +0300

    insi

---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Osama Ibrahim",
+  profile: '/profile.jpg',
+  designation: "Software Developer",
+  description: "My name is OSAMA. I am a full-stack programmer, MERN stack [ Mongooe + Express + ReactJS + NodeJS ]. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving. My core skill is based on JavaScript and I love to do most of the things using JavaScript. I am available for any kind of job opportunity that suits my skills and interests.",
+  email: 'osamaibrahim1948@gmail.com',
+  phone: '+201024276623',
+  address: 'Egypt, Al Garbia, Tanta - Highway 11, Mohammed Dawood Street',
+  github: 'https://github.com/OsamaIbrhim',
+  facebook: 'https://www.facebook.com/4teaman4/',
+  linkedIn: 'https://www.linkedin.com/in/osamaibrhim/',
+  twitter: 'https://x.com/teaman_os',
+  stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+  leetcode: "https://leetcode.com/said3812/",
+  devUsername: "said7388",
+  resume: "https://drive.google.com/file/d/1cLU51uO--S7LpGXuB6JKu0TwJdX2Qlrt/view?usp=sharing"
+}
